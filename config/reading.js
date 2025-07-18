594e8ffc01fddf87af4762a1a0486bdab42245f4
Xalsier <81521840+Xalsier@users.noreply.github.com>
2025-07-17 21:53:23 -0400

    Better formatting for front page

--- config/reading.js
+++ config/reading.js
@@ -2,10 +2,11 @@ const reviews = [
     { author: 'Iris Chang', title: 'The Rape of Nanking: The Forgotten Holocaust of World War II', rating: 5, tags: ['History'] },
     { author: 'Southwell, Gareth', title: 'What Would Marx Do?: How the Greatest Political Theorists Would Solve Your Everyday Problems.', rating: 4, tags: ['Socialism', 'Philosophy'] },
     { author: 'Marx, Karl', title: 'The Communist Manifesto.', rating: 4, tags: ['Socialism'] },
-    { author: 'Lenin, Vladimir', title: 'The State and Revolution', rating: 3, tags: ['Socialism'] },
     { author: 'Ross, Kristin', title: 'Communal Luxury: The Political Imaginary of the Paris Commune', rating: 3, tags: ['Socialism', 'History'] },
+    { author: 'Lenin, Vladimir', title: 'The State and Revolution', rating: 3, tags: ['Socialism'] },
     { author: 'Freire, Paulo', title: 'Pedagogy of the Oppressed', rating: 2, tags: ['Philosophy'] },
     { author: 'Kyabgon, Traleg', title: "Karma: What It Is, What It Isn't, Why It Matters", rating: 2, tags: ['Philosophy'] },
+    { author: 'Aurelius, Marcus', title: "The Emperor's Handbook", rating: 2, tags: ['Philosophy'] },
     { author: 'Engels, Friedrich', title: 'Socialism: Utopian and Scientific', rating: 1, tags: ['Socialism'] },
     { author: 'Machiavelli, Niccolò', title: 'The Prince', rating: 1, tags: ['Philosophy'] },
     { author: 'Seneca', title: 'How to Do the Right Thing: An Ancient Guide to Treating People Fairly', rating: 1, tags: ['Philosophy'] }
@@ -13,5 +14,4 @@ const reviews = [
   
   const currentlyReading = [
     { title: "Orientalism by Edward Said", rating: 5 },
-    { title: "The Emperor's Handbook by Marcus Aurelius", rating: 1 }
   ];
