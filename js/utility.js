8f9f7159502782eae758d1033eb79acda165ae6b
Xalsier <81521840+Xalsier@users.noreply.github.com>
2025-12-09 13:14:26 -0500

    Notice update

--- js/utility.js
+++ js/utility.js
@@ -45,6 +45,7 @@ function navigate(select) {
 
 function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
+
   if (section) {
     section.scrollIntoView({ behavior: "smooth" });
   }
