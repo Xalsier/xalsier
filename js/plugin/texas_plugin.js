dfa0edd0f74759976a2fe7f190def8ca069f0d47
Xalsier <81521840+Xalsier@users.noreply.github.com>
2026-09-13 22:47:26 -0400

    Watchpage test.

--- js/plugin/texas_plugin.js
+++ js/plugin/texas_plugin.js
@@ -1,17 +1,76 @@
-function showFormat(format) {
-
-    document.getElementById("player-audiobook").style.display = "none";
-    document.getElementById("player-visualnovel").style.display = "none";
-    document.getElementById("player-shortstory").style.display = "none";
-    document.getElementById("player-book").style.display = "none";
-
-    document.getElementById("player-" + format).style.display = "block";
-
+const formatTemplates = {
+    audiobook: `
+      <iframe
+        width="900"
+        height="520"
+        src="https://www.youtube-nocookie.com/embed/b_TCfRGj_lI"
+        title="A Fragging in Texas ✿ Narrated by Xalsier"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+        style="width:100%;max-width:900px;">
+      </iframe>`,
+      
+    visualnovel: `
+      <iframe
+        frameborder="0"
+        src="https://itch.io/embed-upload/19118148?color=000000"
+        allowfullscreen
+        width="900"
+        height="520"
+        style="width:100%;max-width:900px;">
+        <a href="https://xalsier.itch.io/a-fragging-in-texas">Play A Fragging in Texas on itch.io</a>
+      </iframe>
+      <br>
+      <center style="padding:20px;text-align:center;opacity:0.5;">
+        (Note, if a 429 error occurs due to too many people playing the game at once, try one of the below links)
+      </center>
+      <center>
+        ✿ <a href="https://xalsier.itch.io/a-fragging-in-texas">Itch.io</a>
+        ✿ <a href="https://www.newgrounds.com/portal/view/1049962">Newgrounds</a>
+        ✿ <a href="https://play.unity.com/en/games/44dd674e-3340-44eb-9ca2-6f51e81b55a8/webglmedaldebug2">Unity Play</a> ✿
+      </center>`,
+  
+    shortstory: `
+      <div style="padding:40px 20px;text-align:center;">
+        <p>
+          ✿ <a href="https://www.furaffinity.net/view/64039266/">Fur Affinity</a>
+          ✿ <a href="https://www.pixiv.net/novel/show.php?id=28638645">Pixiv</a>
+          ✿ <a href="https://substack.com/home/post/p-208105623">Substack</a> ✿
+        </p><br>
+        <p>
+          ✿ <a href="https://archiveofourown.org/works/91380881">AO3</a>
+          ✿ <a href="https://sofurry.com/s/e5RRqNAm">SoFurry</a>
+          ✿ <a href="https://www.deviantart.com/xalsier/art/A-Fragging-in-Texas-1300148731">DeviantArt</a> ✿
+        </p><br>
+        <p>
+          ✿ <a href="https://www.weasyl.com/~xalsier/submissions/2614721/a-fragging-in-texas">Weasyl</a>
+          ✿ <a href="https://www.tumblr.com/xalsier/827185351810498560/a-fragging-in-texas">Tumblr</a> ✿
+        </p>
+      </div>`,
+  
+    book: `
+      <div style="padding:40px 20px;text-align:center;">
+        <center><a href="https://www.goodreads.com/book/show/255582193"><img src="./thumb/goodreads_thumb.png"></a></center>
+        <br>
+        <p>
+          ✿ <a href="https://www.amazon.com/dp/B0H9C2BNC7">Kindle</a>
+          ✿ <a href="https://play.google.com/store/books/details?id=qpv1EQAAQBAJ">Google Play</a> ✿
+        </p>
+      </div>`
+  };
+  
+  function showFormat(format) {
+    const container = document.getElementById("media-container");
+    if (!container || !formatTemplates[format]) return;
+    container.innerHTML = formatTemplates[format];
     document.querySelectorAll(".texas-btn").forEach(button => {
-        button.classList.remove("active");
-        button.classList.add("inactive");
+      button.classList.remove("active");
+      button.classList.add("inactive");
     });
-
-    document.getElementById("button-" + format).classList.remove("inactive");
-    document.getElementById("button-" + format).classList.add("active");
-}
+    const activeBtn = document.getElementById("button-" + format);
+    if (activeBtn) {
+      activeBtn.classList.remove("inactive");
+      activeBtn.classList.add("active");
+    }
+  }
